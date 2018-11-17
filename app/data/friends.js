23ee7f25555ed68992ba17546edde4e0a3302ae9
Zack <zack1444@gmail.com>
2018-11-17 12:49:45 -0800

    adding pictures

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,148 +2,147 @@
 
 var friends = [
     {name:'Olivia',
-     photo:'https://www.oliverwyman.com/content/dam/oliver-wyman/v2/careers/profiles/juink.jpg',
+     photo:'https://www.dropbox.com/s/gmr77owzpnzy2j2/michelle.jpeg?dl=0',
      scores: [
         1,	1,	4,	3,	4,	1,	5,	3,	5,	2
  
      ]
-    },
-    {name:'Sarah',
-     photo:'https://www.oliverwyman.com/content/dam/oliver-wyman/v2/careers/profiles/juink.jpg',
-     scores: [
-        4,
-        5,
-        3,
-        1,
-        4,
-        4,
-        3,
-        3,
-        4,
-        4        
+     }
+//     {name:'Sarah',
+//      photo:'https://www.oliverwyman.com/content/dam/oliver-wyman/v2/careers/profiles/juink.jpg',
+//      scores: [
+//         4, 5,
+//         3,
+//         1,
+//         4,
+//         4,
+//         3,
+//         3,
+//         4,
+//         4        
 
-     ]
-    },
-    {name:'Christina',
-     photo:'https://www.oliverwyman.com/content/dam/oliver-wyman/v2/careers/profiles/juink.jpg',
-     scores: [
-        5,
-        4,
-        4,
-        2,
-        3,
-        2,
-        2,
-        2,
-        5,
-        4    
-     ]
-    },
-    {name:'Jenny',
-     photo:'https://www.oliverwyman.com/content/dam/oliver-wyman/v2/careers/profiles/juink.jpg',
-     scores: [
-        5,
-        5,
-        5,
-        5,
-        4,
-        5,
-        2,
-        3,
-        3,
-        2       
-     ]
-    },
-    {name:'Stacy',
-     photo:'https://www.oliverwyman.com/content/dam/oliver-wyman/v2/careers/profiles/juink.jpg',
-     scores: [
-        4,
-        5,
-        4,
-        5,
-        1,
-        2,
-        3,
-        3,
-        5,
-        2
-     ]
-    },
-    {name:'Emma',
-     photo:'https://www.oliverwyman.com/content/dam/oliver-wyman/v2/careers/profiles/juink.jpg',
-     scores: [
-        2,
-        3,
-        4,
-        4,
-        3,
-        3,
-        1,
-        1,
-        4,
-        5      
-     ]
-    },
-    {name:'Rachel',
-     photo:'https://www.oliverwyman.com/content/dam/oliver-wyman/v2/careers/profiles/juink.jpg',
-     scores: [
-        4,
-        3,
-        2,
-        5,
-        1,
-        4,
-        1,
-        5,
-        1,
-        1
-     ]
-    },
-    {name:'Chris',
-     photo:'https://www.oliverwyman.com/content/dam/oliver-wyman/v2/careers/profiles/juink.jpg',
-     scores: [
-        2,
-        3,
-        2,
-        1,
-        5,
-        4,
-        3,
-        5,
-        4,
-        3
-     ]
-    },
-    {name:'Ben',
-     photo:'https://www.oliverwyman.com/content/dam/oliver-wyman/v2/careers/profiles/juink.jpg',
-     scores: [
-        2,
-        3,
-        1,
-        2,
-        2,
-        4,
-        5,
-        5,
-        1,
-        4
-     ]
-    },
-    {name:'Frank',
-    photo:'https://www.oliverwyman.com/content/dam/oliver-wyman/v2/careers/profiles/juink.jpg',
-    scores: [
-        1,
-        1,
-        5,
-        4,
-        2,
-        2,
-        3,
-        4,
-        5,
-        4  
-    ]
-   },
+//      ]
+//     },
+//     {name:'Christina',
+//      photo:'https://www.oliverwyman.com/content/dam/oliver-wyman/v2/careers/profiles/juink.jpg',
+//      scores: [
+//         5,
+//         4,
+//         4,
+//         2,
+//         3,
+//         2,
+//         2,
+//         2,
+//         5,
+//         4    
+//      ]
+//     },
+//     {name:'Jenny',
+//      photo:'https://www.oliverwyman.com/content/dam/oliver-wyman/v2/careers/profiles/juink.jpg',
+//      scores: [
+//         5,
+//         5,
+//         5,
+//         5,
+//         4,
+//         5,
+//         2,
+//         3,
+//         3,
+//         2       
+//      ]
+//     },
+//     {name:'Stacy',
+//      photo:'https://www.oliverwyman.com/content/dam/oliver-wyman/v2/careers/profiles/juink.jpg',
+//      scores: [
+//         4,
+//         5,
+//         4,
+//         5,
+//         1,
+//         2,
+//         3,
+//         3,
+//         5,
+//         2
+//      ]
+//     },
+//     {name:'Emma',
+//      photo:'https://www.oliverwyman.com/content/dam/oliver-wyman/v2/careers/profiles/juink.jpg',
+//      scores: [
+//         2,
+//         3,
+//         4,
+//         4,
+//         3,
+//         3,
+//         1,
+//         1,
+//         4,
+//         5      
+//      ]
+//     },
+//     {name:'Rachel',
+//      photo:'https://www.oliverwyman.com/content/dam/oliver-wyman/v2/careers/profiles/juink.jpg',
+//      scores: [
+//         4,
+//         3,
+//         2,
+//         5,
+//         1,
+//         4,
+//         1,
+//         5,
+//         1,
+//         1
+//      ]
+//     },
+//     {name:'Chris',
+//      photo:'https://www.oliverwyman.com/content/dam/oliver-wyman/v2/careers/profiles/juink.jpg',
+//      scores: [
+//         2,
+//         3,
+//         2,
+//         1,
+//         5,
+//         4,
+//         3,
+//         5,
+//         4,
+//         3
+//      ]
+//     },
+//     {name:'Ben',
+//      photo:'https://www.oliverwyman.com/content/dam/oliver-wyman/v2/careers/profiles/juink.jpg',
+//      scores: [
+//         2,
+//         3,
+//         1,
+//         2,
+//         2,
+//         4,
+//         5,
+//         5,
+//         1,
+//         4
+//      ]
+//     },
+//     {name:'Frank',
+//     photo:'https://www.oliverwyman.com/content/dam/oliver-wyman/v2/careers/profiles/juink.jpg',
+//     scores: [
+//         1,
+//         1,
+//         5,
+//         4,
+//         2,
+//         2,
+//         3,
+//         4,
+//         5,
+//         4  
+//     ]
+//    },
 
 ];
 
